3f4965b3b8df464cdbff615259dc8ac7930b1028
Sscnrj <remi.jegard@gmail.com>
2025-02-24 14:26:04 +0100

    tp3

--- src/main/java/fr/remi/jegard/demojpa/Main.java
+++ src/main/java/fr/remi/jegard/demojpa/Main.java
@@ -1,13 +1,35 @@
 package fr.remi.jegard.demojpa;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import fr.remi.jegard.demojpa.bo.Emprunt;
+import fr.remi.jegard.demojpa.bo.Livre;
+import jakarta.persistence.*;
 
 public class Main {
     public static void main(String[] args) {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa")) {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
 
-        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa")) {
-            System.out.println("EntityManagerFactory created");
+
+            Emprunt emprunt = em.find(Emprunt.class, 1);
+            if (emprunt != null) {
+                System.out.println("Emprunt trouvé, ID : " + emprunt.getId());
+                System.out.println("Livres empruntés : ");
+                for (Livre livre : emprunt.getLivres()) {
+                    System.out.println("- " + livre.getTitre() + " par " + livre.getAuteur());
+                }
+            }
+
+            // Exemple : récupérer tous les emprunts d'un client donné (id=1)
+            TypedQuery<Emprunt> query = em.createQuery(
+                    "SELECT e FROM Emprunt e WHERE e.client.id = :clientId", Emprunt.class);
+            query.setParameter("clientId", 1);
+            query.getResultList().forEach(e ->
+                    System.out.println("Emprunt ID : " + e.getId() + ", Date début : " + e.getDateDebut())
+            );
+
+            em.getTransaction().commit();
+            em.close();
         }
     }
 }
---
+++ src/main/java/fr/remi/jegard/demojpa/bo/Livre.java
@@ -0,0 +1,63 @@
+package fr.remi.jegard.demojpa.bo;
+
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "TITRE")
+    private String titre;
+
+    @Column(name = "AUTEUR")
+    private String auteur;
+
+    @ManyToMany(mappedBy = "livres")
+    private List<Emprunt> emprunts;
+
+    public Livre() {}
+
+    public Livre(String titre, String auteur) {
+        this.titre = titre;
+        this.auteur = auteur;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public List<Emprunt> getEmprunts() {
+        return emprunts;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setEmprunts(List<Emprunt> emprunts) {
+        this.emprunts = emprunts;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                '}';
+    }
+}
